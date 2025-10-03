e958ef3939ee5eb0fa2faa72457728da8c031507
Stoneface-Vimes <cliffweir92@gmail.com>
2025-10-02 20:49:35 -0400

    added word count for the text of a .txt file

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ def get_book_text(filepath):
         text = f.read()
     return text
 
+def get_number_of_words(text):
+    number_of_words = text.split()
+    return len(number_of_words)
+
 def main():
-    print(get_book_text("books/frankenstein.txt"))
+    book_text = get_book_text("books/frankenstein.txt")
+    # print(book_text)
+    print(f"Found {get_number_of_words(book_text)} total words")
     
 main()
